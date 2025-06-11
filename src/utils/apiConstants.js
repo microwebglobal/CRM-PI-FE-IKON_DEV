5c2b1c98de22626a97aeec92f99b5509cd1b5fb0
Sahiru <64680100+sahiru123@users.noreply.github.com>
2025-06-12 04:43:01 +0530

    Update apiConstants.js

--- src/utils/apiConstants.js
+++ src/utils/apiConstants.js
@@ -1,5 +1,5 @@
 // apiConstants.js
-export const BASE_URL=process.env.REACT_APP_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://pidash.microwebstudios.com";
 
 // Endpoints
 export const ENDPOINTS = {
